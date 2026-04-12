403fec7719313a158044777c2ce3d91fef301ba0
CWorld <cworld0@qq.com>
2026-04-13 00:50:07 +0800

    fix(package): :bug: fix footer template

--- src/site.config.ts
+++ src/site.config.ts
@@ -79,7 +79,8 @@ export const theme: ThemeUserConfig = {
     credits: true,
     /** Optional details about the social media accounts for this site. */
     social: [
-      { icon: 'github', label: 'GitHub', href: 'https://github.com/cworld1/astro-theme-pure' }
+      { icon: 'github', label: 'GitHub', href: 'https://github.com/cworld1/astro-theme-pure' },
+      { icon: 'rss', label: 'RSS', href: '/rss.xml' }
     ]
   },
 
